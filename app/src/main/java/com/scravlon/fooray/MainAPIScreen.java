fb89c2dfd94d4c4ea2c6be625465fe56b8544cb3
Scravlon <haokokyong10@gmail.com>
2019-11-02 22:03:57 -0400

    Splash screen done, API call

---
+++ app/src/main/java/com/scravlon/fooray/MainAPIScreen.java
@@ -0,0 +1,58 @@
+package com.scravlon.fooray;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainAPIScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        String url ="https://api.wegmans.io/meals/recipes?api-version=2018-10-18";
+        url = url + "&Subscription-Key="+constantClass.subKey;
+        apiCall(url);
+
+
+
+    }
+
+
+    public void apiCall(String urlReq){
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlReq,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("respond",response);
+                        Toast.makeText(MainAPIScreen.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+}
